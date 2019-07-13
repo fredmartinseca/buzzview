5b2b6554bd500b71b2fcfeb7b05e3f0736d37c0d
fredmartinseca <fredmartinseca@gmail.com>
2019-07-13 14:56:30 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/faez/BuzzviewApplication.java
@@ -0,0 +1,13 @@
+package br.com.faez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuzzviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuzzviewApplication.class, args);
+	}
+
+}
