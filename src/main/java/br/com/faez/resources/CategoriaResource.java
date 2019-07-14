caca62f3b06b2e12f87b1a1b0a85d039091f577d
fredmartinseca <fredmartinseca@gmail.com>
2019-07-13 23:19:02 -0300

    Testando Categoria e rest

--- src/main/java/br/com/faez/resources/CategoriaResource.java
+++ src/main/java/br/com/faez/resources/CategoriaResource.java
@@ -1,16 +1,29 @@
 package br.com.faez.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.faez.entity.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
-	
-	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Rest funcionando";
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Rações");
+		Categoria cat2 = new Categoria(2, "Brinquedo Pet");
+
+		List<Categoria> lista = new ArrayList<Categoria>();
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
 
 }
