7b94dcc595a5bc5bdcfc563e816d59cb9e5fb87d
fredmartinseca <fredmartinseca@gmail.com>
2019-07-14 00:33:21 -0300

    Integrando JPA e banco de teste H2

--- src/main/java/br/com/faez/entity/Categoria.java
+++ src/main/java/br/com/faez/entity/Categoria.java
@@ -2,9 +2,17 @@ package br.com.faez.entity;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 
@@ -27,6 +35,17 @@ public class Categoria implements Serializable {
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public Boolean isNome() {
+		if (nome == null) {
+			return false;
+		} else if (nome.trim().length() < 2) {
+			return false;
+		} else {
+			return true;
+		}
+
 	}
 
 	public String getNome() {
