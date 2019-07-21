46701d7c3b72474bcbd49dbb771260fb06a9accd
fredmartinseca <fredmartinseca@gmail.com>
2019-07-21 15:18:27 -0300

    Criação de endpoint cliente e resolução de referencia ciclica json

---
+++ src/main/java/br/com/faez/buzzview/services/ClienteService.java
@@ -0,0 +1,24 @@
+package br.com.faez.buzzview.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.faez.buzzview.entity.Cliente;
+import br.com.faez.buzzview.repositories.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	public Cliente buscar(Integer id) {
+		Cliente obj = repo.findOne(id);
+		if (obj == null) {
+		 throw new br.com.faez.buzzview.services.exception.ObjectNotFoundException("Objeto não encontrado! Id: " + id
+		+ ", Tipo: " + Cliente.class.getName());
+		}
+		return obj;
+	}
+
+}
